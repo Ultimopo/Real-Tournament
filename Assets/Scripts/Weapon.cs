463b6bcf1ee915f5df2ced29df19bd2f9a1941d9
Oskaridzas <146350511+Ultimopo@users.noreply.github.com>
2024-02-22 16:35:14 +0200

    Shotgun + clips system

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -17,8 +17,15 @@ public class Weapon : MonoBehaviour
     bool isReloading = false;
     public bool isAutomatic;
 
+    public bool hasClips;
+    public int clipsLeft = 7;
+
+    public bool isMultiBullet;
+    public int PelletCount = 12;
+
     public TextMeshProUGUI ammoLeftText;
     public TextMeshProUGUI MaxAmmoText;
+    public TextMeshProUGUI clipsAmountText;
 
 
     void Start()
@@ -43,6 +50,16 @@ public class Weapon : MonoBehaviour
             Shoot();
         }
 
+        if(isMultiBullet && Input.GetKeyDown(KeyCode.Mouse0) && ammoLeft != 0)
+        {
+            for (int i = 0; i < PelletCount; i++)
+            {
+
+                Shoot();
+            }
+            ammoLeft--;
+        }
+
         if (Input.GetKeyDown(KeyCode.R) && isReloading == false)
         {
             Reload();
@@ -52,22 +69,42 @@ public class Weapon : MonoBehaviour
         {
             timeToReload -= Time.deltaTime;
         }
-        if (timeToReload <= 0)
+        if (timeToReload <= 0 && hasClips)
         {
             ammoLeft = maxAmmo;
             timeToReload = reloadTime;
             isReloading = false;
+            clipsLeft = clipsLeft - maxAmmo;
         }
+        if (timeToReload <= 0 && !hasClips)
+        {
+            if(ammoLeft != maxAmmo)
+            {
+                ammoLeft++;
+                clipsLeft--;
+            }
+            else
+            {
+                isReloading = false;
+            }
+            timeToReload = reloadTime;
+            
+        }
+
         shootCooldown -= Time.deltaTime;
 
         ammoLeftText.text = ammoLeft.ToString();
         MaxAmmoText.text = maxAmmo.ToString();
+        clipsAmountText.text = clipsLeft.ToString();
     }
 
     void Shoot()
     {
         Instantiate(bulletPrefab, transform.position, transform.rotation);
-        ammoLeft--;
+        if (!isMultiBullet)
+        {
+            ammoLeft--;
+        }
 
         if (shootCooldown > 0) return;
         shootCooldown = shootInterval;
